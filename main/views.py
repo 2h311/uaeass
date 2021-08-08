81e6136986994603c55124a757c261a51aeb1f12
boldman <aj.bold14@gmail.com>
2021-08-08 14:14:24 +0100

    Second commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView, FormView
 
 # Create your views here.
+
+
+class IndexView(TemplateView):
+	'''
+	The home page and the likes 
+	'''
+	template_name = 'main/index.html'
+	def get_context_data(self, *args, **kwargs):
+		return super().get_context_data()
+
+
+class ProcessAssignmentView(FormView):
+	'''
+	Sends email data to the receiver 
+	as well as stores in DB. 
+	so , definitely, model is required for the form data 
+	'''
+
+	template_name = '' 
+
+
+	def form_valid(self, form):
+		'''
+		send email and store the data 
+		'''
+		pass
